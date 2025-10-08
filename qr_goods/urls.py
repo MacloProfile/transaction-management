ce2034c5261a62d1a3e21faf7ee03443e1e6db34
Maclo <mr.lobzuk@gmail.com>
2025-10-08 17:07:22 +0300

    add products list

--- qr_goods/urls.py
+++ qr_goods/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 
 from qr_goods.views.generate_qr import generate_qr
 from qr_goods.views.image_product_by_link import product_by_token
+from qr_goods.views.show_products_list import show_products
 
 app_name = "qr_goods"
 
 urlpatterns = [
     path("qr/", generate_qr, name="qr_generator"),
-    path("qr/product/<uuid:token>/", product_by_token, name="product_by_token")
+    path("qr/product/<uuid:token>/", product_by_token, name="product_by_token"),
+    path("products", show_products, name="show_products")
 ]
---
+++ qr_goods/views/show_products_list.py
@@ -0,0 +1,25 @@
+from django.core.paginator import Paginator
+from django.shortcuts import render
+from integration_utils.bitrix24.bitrix_user_auth.main_auth import main_auth
+
+
+@main_auth(on_cookies=True)
+def show_products(request):
+    bitrix_token = request.bitrix_user_token
+
+    products_response = bitrix_token.call_api_method(
+        "crm.product.list",
+        {
+            "select": ["ID", "NAME", "PRICE", "CURRENCY_ID"],
+            "order": {"NAME": "ASC"},
+            "start": 0
+        }
+    )
+
+    products = products_response.get("result", [])
+
+    paginator = Paginator(products, 1)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, "qr_generator/products_list.html", {"page_obj": page_obj})
