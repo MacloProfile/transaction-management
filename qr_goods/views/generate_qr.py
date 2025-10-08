b92e19d1e92eea26b853005794fa4682a53235b6
Maclo <mr.lobzuk@gmail.com>
2025-10-08 15:24:56 +0300

    add incorrect product error

--- qr_goods/views/generate_qr.py
+++ qr_goods/views/generate_qr.py
@@ -12,11 +12,23 @@ from qr_goods.services.qr_generator import generate_qr_image
 def generate_qr(request):
     bitrix_token = request.bitrix_user_token
 
+    products_response = bitrix_token.call_api_method(
+        "crm.product.list",
+        {
+            "select": ["ID", "NAME", "PRICE", "CURRENCY_ID"],
+            "order": {"NAME": "ASC"},
+            "start": 0
+        }
+    )
+
+    products = products_response.get("result", [])
+
     if request.method == "POST":
         product_id = request.POST.get("product_id")
 
         if not product_id:
-            return render(request, "qr_generator/generate.html", {"error": "Не выбран товар"})
+            return render(request, "qr_generator/generate.html",
+                          {"error": "Товар не найден", "products": products})
 
         response = bitrix_token.call_api_method(
             "crm.product.get",
@@ -41,22 +53,11 @@ def generate_qr(request):
         )
 
         return render(request, "qr_generator/generate.html", {
+            "products": products,
             "product": product,
             "token": token,
             "qr_link": qr_link,
             "qr_base64": qr_base64
         })
-
-    products_response = bitrix_token.call_api_method(
-        "crm.product.list",
-        {
-            "select": ["ID", "NAME", "PRICE", "CURRENCY_ID"],
-            "order": {"NAME": "ASC"},
-            "start": 0
-        }
-    )
-
-    products = products_response.get("result", [])
-    print(products)
 
     return render(request, "qr_generator/generate.html", {"products": products})
